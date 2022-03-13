c22fac82c15725d3adf2f784d90ca32a26c4b3f8
AssassinAFS <andrefelipe.c.santos@gmail.com>
2022-03-13 00:16:56 -0300

    send MforCM

---
+++ ConvertMetrosEmCentímetros.java
@@ -0,0 +1,15 @@
+package git;
+import java.util.Scanner;
+
+public class ConvertMetrosEmCentímetros {
+
+	static Scanner scan = new Scanner(System.in);
+		
+	public static void main(String[] args) {
+		System.out.println("Converter Quanto(s) Metro(s) Para Centimetros?");
+			float metro = scan.nextFloat();
+			float centimetro = (metro)*100;
+		
+			System.out.println(metro + " Metro(s)" + " = " + centimetro + " Centimetros");
+	}
+}
